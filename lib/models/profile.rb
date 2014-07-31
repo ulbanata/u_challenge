6df779bec6b003bf6a66e8efe484ad507f09c12d
Patrick Brennan <chief125@gmail.com>
2014-07-31 04:25:06 +0000

    Added ability to connect and disconnect profiles to and from brands.

--- lib/models/profile.rb
+++ lib/models/profile.rb
@@ -2,7 +2,27 @@ class Profile < ActiveRecord::Base
   has_many :brand_profiles
   has_many :brands, :through => :brand_profiles
 
-  def self.add_profile
+  def self.create_profile
     Profile.create
+  end
+
+  def connect_brand(brand_id)
+    #Using .find_by to ignore error thrown by .find if id not found
+    brand = Brand.find_by(id: brand_id)
+    if brand
+      self.brands << brand
+    else
+      nil
+    end
+  end
+
+  def disconnect_brand(brand_id)
+    #Using .find_by to ignore error thrown by .find if id not found
+    brand = Brand.find_by(id: brand_id)
+    if brand
+      self.brands.delete(brand)
+    else
+      nil
+    end
   end
 end
--- spec/models_spec/profile_spec.rb
+++ spec/models_spec/profile_spec.rb
@@ -5,10 +5,38 @@ describe Profile do
     expect(Profile).to be_a(Class)
   end
 
-  describe '.add_profile' do
+  describe '.create_profile' do
     it 'should create a profile' do
-      profile = Profile.add_profile
+      profile = Profile.create_profile
       expect(profile).to be_a(Profile)
+    end
+  end
+
+  describe '#connect_brand' do
+    it 'should connect a brand to a profile' do
+      profile = Profile.create_profile
+      brand = Brand.create_brand({name: "Pepsi"})
+      profile.connect_brand(brand.id)
+      expect(profile.brands.last).to eq(brand)
+      expect(brand.profiles.last).to eq(profile)
+    end
+
+    it 'should return nil if a brand id is passed in that doesn\'t exist' do
+      profile = Profile.create_profile
+      result = profile.connect_brand(Brand.last.id + 1)
+      expect(result).to be_nil
+    end
+  end
+
+  describe '#disconnect_brand' do
+    it 'should disconnect a brand to a profile' do
+      profile = Profile.create_profile
+      brand = Brand.create_brand({name: "HEB"})
+      profile.connect_brand(brand.id)
+      expect(profile.brands.last).to eq(brand)
+      profile.disconnect_brand(brand.id)
+      expect(profile.brands.empty?).to eq(true)
+      expect(brand.profiles.empty?).to eq(true)
     end
   end
 end
