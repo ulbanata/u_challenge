c731556774b98005ac8c925ee987ee2a970f7112
Patrick Brennan <chief125@gmail.com>
2014-07-31 23:26:53 +0000

    Added basic error handling

--- lib/request_handler.rb
+++ lib/request_handler.rb
@@ -4,18 +4,23 @@ class RequestHandler
   @@dc = Dalli::Client.new('localhost:11211', options)
 
   def self.grab_profile(id)
-    @@dc.fetch(["profile", id]) { Profile.find(id).to_json(:include => :brands) }
+    @@dc.fetch(["profile", id]) {
+      profile = Profile.find_by(id: id)
+      return {error: "No profile with #{id} exists!"}.to_json unless profile
+      profile.to_json(:include => :brands)
+    }
   end
 
   def self.create_profile(data)
     new_profile = Profile.create_profile
     self.connect_brands(data["add"], new_profile) if data["add"]
-    reset_profile_cache(id)
-    self.grab_profile(id)
+    reset_profile_cache(new_profile.id)
+    self.grab_profile(new_profile.id)
   end
 
   def self.update_profile(data, id)
     profile = Profile.find_by(id: id)
+    return {error: "No profile with #{id} exists!"}.to_json unless profile
     self.connect_brands(data["add"], profile) if data["add"]
     self.disconnect_brands(data["remove"], profile) if data["remove"]
     reset_profile_cache(id)
@@ -23,7 +28,11 @@ class RequestHandler
   end
 
   def self.grab_brand(id)
-    @@dc.fetch(["brand", id]) { Brand.find(id).to_json(:include => :profiles) }
+    @@dc.fetch(["brand", id]) {
+      brand = Brand.find_by(id: id)
+      return {error: "No brand with #{id} exists!"}.to_json unless brand
+      brand.to_json(:include => :profiles)
+    }
   end
 
   def self.create_brand(name)
